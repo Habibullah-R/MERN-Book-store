c37669be5f7f0431d77a38c3da270f05225e95f4
Habibullah <habibullahrashid150@gmail.com>
2024-04-11 12:06:29 +0500

    done

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({withcredentials:true,origin:'https://mern-book-store-sand.vercel.app'}))
+app.use(cors({withcredentials:true,origin:['https://mern-book-store-sand.vercel.app']}))
 
 db()
 
